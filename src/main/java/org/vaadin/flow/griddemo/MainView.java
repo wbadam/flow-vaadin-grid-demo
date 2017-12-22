b3c215b4f14268b9cfa35fdd2ee11b9a852f896a
Adam Wagner <adam@vaadin.com>
2017-12-22 23:45:40 +0200

    Create example grid with person data

--- src/main/java/org/vaadin/flow/griddemo/MainView.java
+++ src/main/java/org/vaadin/flow/griddemo/MainView.java
@@ -15,9 +15,16 @@
  */
 package org.vaadin.flow.griddemo;
 
+import org.vaadin.flow.griddemo.data.Person;
+import org.vaadin.flow.griddemo.data.PersonService;
+
 import com.vaadin.router.Route;
 import com.vaadin.ui.common.HtmlImport;
+import com.vaadin.ui.grid.ColumnGroup;
+import com.vaadin.ui.grid.Grid;
+import com.vaadin.ui.grid.Grid.Column;
 import com.vaadin.ui.layout.VerticalLayout;
+import com.vaadin.ui.renderers.TemplateRenderer;
 
 /**
  * The main view contains a button and a template element.
@@ -26,7 +33,67 @@ import com.vaadin.ui.layout.VerticalLayout;
 @Route("")
 public class MainView extends VerticalLayout {
 
-    public MainView() {
+    private Grid<Person> grid;
 
+    public MainView() {
+        grid = new Grid<>();
+
+        // Columns
+        Column<Person> indexColumn = grid
+                .addColumn(TemplateRenderer.of("[[index]]")).setHeader("#");
+        Column<Person> firstNameColumn = grid.addColumn(Person::getFirstName)
+                .setHeader("First name");
+        Column<Person> lastNameColumn = grid.addColumn(Person::getLastName)
+                .setHeader("Last name");
+        Column<Person> streetColumn = grid
+                .addColumn(person -> person.getAddress().getStreet())
+                .setHeader("Street");
+        Column<Person> cityColumn = grid
+                .addColumn(person -> person.getAddress().getCity())
+                .setHeader("City");
+        Column<Person> stateColumn = grid
+                .addColumn(person -> person.getAddress().getState())
+                .setHeader("State");
+        Column<Person> zipColumn = grid
+                .addColumn(person -> person.getAddress().getZip())
+                .setHeader("Zip");
+        Column<Person> countryColumn = grid
+                .addColumn(person -> person.getAddress().getCountry())
+                .setHeader("Country");
+        Column<Person> phoneColumn = grid
+                .addColumn(person -> person.getAddress().getPhone())
+                .setHeader("Phone");
+        Column<Person> emailColumn = grid.addColumn(Person::getEmail)
+                .setHeader("Email");
+
+        // Column groups
+        ColumnGroup nameGroup = grid
+                .mergeColumns(firstNameColumn, lastNameColumn)
+                .setHeader("Name");
+        ColumnGroup addressGroup = grid
+                .mergeColumns(streetColumn, cityColumn, stateColumn, zipColumn,
+                        countryColumn).setHeader("Address");
+        ColumnGroup contactGroup = grid.mergeColumns(phoneColumn, emailColumn)
+                .setHeader("Contact");
+
+        // Data
+        grid.setItems(PersonService.getInstance().getAll());
+
+        // Size
+        indexColumn.setWidth("60px").setResizable(false).setFrozen(true);
+        streetColumn.setWidth("250px");
+        cityColumn.setWidth("150px");
+        stateColumn.setWidth("150px");
+        zipColumn.setWidth("150px");
+        phoneColumn.setWidth("200px");
+        emailColumn.setWidth("300px");
+
+        nameGroup.setResizable(true);
+        addressGroup.setResizable(true);
+        contactGroup.setResizable(true);
+
+        grid.setWidth("600px");
+
+        add(grid);
     }
 }
