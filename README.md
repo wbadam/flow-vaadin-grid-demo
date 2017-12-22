0fc6835bea9ab3303415d5de4482e66fdc71673e
Adam Wagner <adam@vaadin.com>
2017-12-22 19:13:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+[![Gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/vaadin-flow/Lobby#?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
+
+# Skeleton Starter for Vaadin Flow
+
+This project can be used as a starting point to create your own Vaadin Flow application.
+It has the necessary dependencies and files to get you started. 
+
+Import it to the IDE of your choice as a Maven project.
+
+Run using `mvn jetty:run` and open [http://localhost:8080](http://localhost:8080) in browser.
+
+For a Flow application example, please see the [Beverage Buddy App Starter for Flow](https://github.com/vaadin/beverage-starter-flow) instead.
---
+++ src/main/java/org/vaadin/flow/griddemo/Servlet.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.vaadin.flow.griddemo;
+
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.server.VaadinServletConfiguration;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * The main servlet for the application.
+ * <p>
+ * It is not mandatory to have the Servlet, since Flow will automatically register a Servlet to any app with at least one {@code @Route} to server root context.
+ */
+@WebServlet(urlPatterns = "/*", name = "UIServlet", asyncSupported = true)
+@VaadinServletConfiguration(productionMode = false)
+public class Servlet extends VaadinServlet {
+}
